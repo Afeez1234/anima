6bd78272d6ac9cc6bd48005ae163405017e3cb95
Afeez1234 <90616642+Afeez1234@users.noreply.github.com>
2022-09-05 05:38:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// function Movel() {
+//     var elem = document.getElementById('sphere1');
+//     var pos= 0;
+//     var id1 = setInterval(frame1, 5);
+//     function frame1(){
+//         if(pos = 90) {
+//             clearInterval(id1);
+//         }else{
+//             pos++;
+//             elem.style.left= pos + 'px';
+//             elem.style.top = pos + 'px';
+//             elem.style.right = pos + 'px';
+//             elem.style.top = pos + 'px';
+//         }
+//     }
+// }
+
+
+
+// function Movel() {
+//     const sphere = document.getElementById('sphere1');
+//     sphere.style.right = Number(sphere.style.right.slice(0,-2) + 10 + 'px')
+// }
+
+
+var leftNUM = 0;
+
+function Movel(){
+    leftNUM += 20;
+    document.getElementById('sphere1').style.marginLeft = leftNUM + 'px';
+}
+
+function Move2() {
+    leftNUM -=10;
+    document.getElementById('sphere1').style.marginLeft = leftNUM + 'px';
+}
+function Move5() {
+    leftNUM -=10;
+    document.getElementById('sphere3').style.marginLeft = leftNUM + 'px';
+}
+function Move6() {
+    leftNUM +=10;
+    document.getElementById('sphere3').style.marginLeft = leftNUM + 'px';
+}
+
+
+
+
+var rightNUM = 0;
+
+function Move3() {
+    rightNUM -=10;
+    document.getElementById('sphere2').style.marginRight = rightNUM + 'px';
+}
+function Move4() {
+    rightNUM +=10;
+    document.getElementById('sphere2').style.marginRight = rightNUM + 'px';
+}
+
